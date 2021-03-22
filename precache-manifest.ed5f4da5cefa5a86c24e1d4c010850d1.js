3c4f44f9bb4b8d642d912122b991d51fd17e8978
AshrafLobo <ashraflobo@gmail.com>
2021-03-22 08:34:59 +0300

    Initial commit

---
+++ precache-manifest.ed5f4da5cefa5a86c24e1d4c010850d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb05946778b9b34e84259cccb5f8f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f141f93ac4d1e10b448d",
+    "url": "/static/css/2.19e10c4f.chunk.css"
+  },
+  {
+    "revision": "5600c0fbb22f7852861c",
+    "url": "/static/css/main.e71e37fd.chunk.css"
+  },
+  {
+    "revision": "f141f93ac4d1e10b448d",
+    "url": "/static/js/2.be44374c.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/static/js/2.be44374c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5600c0fbb22f7852861c",
+    "url": "/static/js/main.be9a0594.chunk.js"
+  },
+  {
+    "revision": "7968548896b08d2111c7",
+    "url": "/static/js/runtime-main.ac4198f9.js"
+  }
+]);
